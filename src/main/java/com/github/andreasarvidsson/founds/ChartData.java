aa8f97accfdd39aaef7e0f40121868829fe715e1
Andreas Arvidsson <andreas.arvidsson@redpill-linpro.com>
2020-01-09 10:54:45 +0100

    Ignore unknown param in chart data

--- src/main/java/com/github/andreasarvidsson/founds/ChartData.java
+++ src/main/java/com/github/andreasarvidsson/founds/ChartData.java
@@ -1,5 +1,6 @@
 package com.github.andreasarvidsson.founds;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
@@ -8,6 +9,7 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
  * @author Andreas Arvidsson
  */
 @JsonInclude(Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ChartData {
 
     public String name, type, currency, countryCode;
