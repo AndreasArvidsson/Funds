ff226e5a382f9a1fc1d1ad4e1dc4547ee6480994
Andreas Arvidsson <andreas.arvidsson@redpill-linpro.com>
2020-01-14 16:04:10 +0100

    Upd founds list

--- src/main/java/com/github/andreasarvidsson/founds/Main.java
+++ src/main/java/com/github/andreasarvidsson/founds/Main.java
@@ -9,24 +9,25 @@ public class Main {
 
     public static void main(String[] args) throws ProtocolException, MalformedURLException, IOException {
         final PortfolioSummary pension = PortfolioSummary.create("Pension", Arrays.asList(
-                new Pair("Avanza Global", 0.60),
+                new Pair("Avanza Global", 0.65),
                 new Pair("Handelsbanken Gl Småbolag Ind Cri A1 SEK", 0.05),
-                new Pair("Spiltan Aktiefond Investmentbolag", 0.15),
+                new Pair("Spiltan Aktiefond Investmentbolag", 0.10),
                 new Pair("SEB Sverige Indexfond", 0.05),
                 new Pair("PLUS Småbolag Sverige Index", 0.05),
                 new Pair("Avanza Emerging Markets", 0.05),
                 new Pair("Swedbank Robur Access Asien", 0.05)
         ));
         final PortfolioSummary founds = PortfolioSummary.create("Värdepapper", Arrays.asList(
-                new Pair("Avanza Global", 0.45),
-                new Pair("Spiltan Aktiefond Investmentbolag", 0.15),
-                new Pair("PLUS Småbolag Sverige Index", 0.05),
+                new Pair("Avanza Global", 0.48),
+                new Pair("Spiltan Aktiefond Investmentbolag", 0.13),
                 new Pair("Avanza Emerging Markets", 0.05),
-                new Pair("Core Ny Teknik A", 0.06),
-                new Pair("DNB Teknologi A", 0.06),
-                new Pair("SEB Teknologifond", 0.06),
-                new Pair("Swedbank Robur Ny Teknik A", 0.06),
-                new Pair("Swedbank Robur Technology A SEK", 0.06)
+                new Pair("Swedbank Robur Access Asien", 0.04),
+                new Pair("Swedbank Robur Technology A SEK", 0.05),
+                new Pair("Swedbank Robur Ny Teknik A", 0.05),
+                new Pair("Core Ny Teknik A", 0.05),
+                new Pair("SEB Teknologifond", 0.05),
+                new Pair("Skandia Time Global", 0.05),
+                new Pair("Lannebo Teknik", 0.05)
         ));
         pension.print();
         founds.print();
